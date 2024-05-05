26758aa52cf7374b44757c6d0bda05fb0a47fa0c
lukassta <lukas.stavaris@gmail.com>
2024-05-05 22:06:43 +0300

    feat: Added 404 page

---
+++ personal-web/app/[locale]/not-found.tsx
@@ -0,0 +1,7 @@
+import { useTranslations } from "next-intl";
+
+export default function NotFoundPage() {
+  const t = useTranslations("NotFound");
+
+  return <p>{t("description")}</p>;
+}
---
+++ personal-web/app/not-found.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <html lang="en">
+      <body>404: Page not found</body>
+    </html>
+  );
+}
