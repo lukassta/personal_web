50d3717988487a1259287805e629bbbb3556a336
lukassta <lukas.stavaris@gmail.com>
2024-05-07 09:30:10 +0300

    feat: Created my CV page

---
+++ personal-web/app/[locale]/my-cv/page.tsx
@@ -0,0 +1,12 @@
+import { useTranslations } from "next-intl";
+
+export default function Index() {
+  const t = useTranslations("Index");
+  return (
+    <>
+      <section>
+        <div>a</div>
+      </section>
+    </>
+  );
+}
